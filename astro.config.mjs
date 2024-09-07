216a6566ce2accb335e6b43a1c79375809985485
scottysseus <sweidenkopf@gmail.com>
2024-09-07 15:13:26 -0400

    setting up github pages for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  site: "https://scottysseus.github.io/",
   integrations: [mdx(), sitemap(), tailwind()],
 });
